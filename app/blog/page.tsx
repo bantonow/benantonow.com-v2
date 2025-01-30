c61319f14226eae06d4df4d039f0a4b585515aa5
Ben Antonow <antonow@umich.edu>
2025-01-30 15:29:53 +0100

    paul graham quote

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -9,6 +9,7 @@ export default async function BlogPage() {
     return (
         <div className="max-w-4xl mx-auto">
             <h1 className="text-[2.5rem] font-bold text-green mb-6">Blog</h1>
+            <p><em>Writing doesn't just communicate ideas; it generates them.</em> - Paul Graham</p><br /><br />
             <div className="space-y-8">
                 {posts.length === 0 ? (
                     <p className="text-gray-600 italic">No posts yet. Check back soon!</p>
