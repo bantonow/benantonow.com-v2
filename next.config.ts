cda27dc7ee3bf9a21ee61b9728ff85bab6a1c9de
Ben Antonow <antonow@umich.edu>
2025-08-05 22:07:58 -0700

    posthog

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,23 @@ import withMDX from '@next/mdx'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://us-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://us.i.posthog.com/:path*',
+      },
+      {
+        source: '/ingest/flags',
+        destination: 'https://us.i.posthog.com/flags',
+      },
+    ]
+  },
+  skipTrailingSlashRedirect: true,
 }
 
 export default withMDX()(nextConfig)
